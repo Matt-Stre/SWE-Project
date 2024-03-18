2564ddc2cbe8d9d786419e18549e05979836a439
Aiden Zepp <aiden.zepp@protonmail.com>
2024-03-18 15:55:45 -0400

    Forgot to add `Status`.

---
+++ src/backend/Status.h
@@ -0,0 +1,27 @@
+//
+// Created by Aiden Zepp on 2024-03-18.
+//
+
+#ifndef SWE_PROJECT_STATUS_H
+#define SWE_PROJECT_STATUS_H
+
+// std...
+#include <string>
+
+namespace http {
+    enum Status : int {
+        Ok = 200,
+        Created = 201,
+        Accepted = 202,
+
+        BadRequest = 400,
+        Unauthorized = 401,
+        Forbidden = 403,
+        NotFound = 404,
+
+        InternalServerError = 500,
+        NotImplemented = 501,
+    };
+} // http
+
+#endif //SWE_PROJECT_STATUS_H
